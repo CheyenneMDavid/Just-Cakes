3af74b646edeec407e269a2627f89483569ae115
Chey <ctenmanikdavid@gmail.com>
2023-09-07 06:38:56 +0000

    Corrections to allow migration

--- cakes/views.py
+++ cakes/views.py
@@ -1,6 +1,6 @@
 # Import all required modules
 from django.shortcuts import render, get_object_or_404
-from .models import Cake
+from .models import IndividualCake
 
 
 # To view the cake list.
@@ -8,7 +8,7 @@ def cake_list(request):
     """Renders a list of all the cakes"""
 
     # Fetches all the cake objects to display in the cake_list.html
-    cakes = Cake.objects.all()
+    cakes = IndividualCake.objects.all()
     # Returns the cake_list.html
     return render(request, "cakes/cakes_list.html", {"cakes": cakes})
 
@@ -18,5 +18,6 @@ def cake_detail(request, cake_id):
     """Render the view of an indiviual cake"""
 
     # Returns individual cake or returns a 404 error if the cake's not found.
-    cake = get_object_or_404(Cake, id=cake_id)
+    cake = get_object_or_404(IndividualCake, id=cake_id)
+
     return render(request, "cakes/cake_detail.html", {"cake": cake})
