64dc41e54a188bad4dbc36e222caeb0c86a4cb8b
Chey <ctenmanikdavid@gmail.com>
2023-09-08 04:22:24 +0000

    Add models to customer account app

--- customer_accounts/models.py
+++ customer_accounts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class CustomerAccount(models.Model):
+    """
+    Customer account model uses the allauth/django user model which extends
+    the User model with additional fields.
+    """
+
+    # I've kept the character count pretty l because th is only dealing
+    # with UK addresses, so 60 is more than long enough.
+    # The longest town in the uk is in wales and only has 58 letters.
+    phone = models.CharField(max_length=12)
+    address_line_1 = models.CharField(max_length=50)
+    address_line_2 = models.CharField(max_length=60)
+    city = models.CharField(max_length=60)
+    county = models.CharField(max_length=20)
+    post_code = models.CharField(max_length=15)
+
+    # non-active until app is extended in functionality.
+    account_number = models.CharField(max_length=20, unique=True)
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+    # non-active until app is extended in functionality.
+    payment_method = models.CharField(max_length=50)
+
+    # non-active until app is extended in functionality.
+    basket_item_count = models.IntegerField(default=0)
+
+    # Links the CustomerAccount model to the django-allauth.
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # Returns the username.
+    def __str__(self):
+        return self.user.username
