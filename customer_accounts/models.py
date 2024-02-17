ef088f5e6e1585fe9c3b8f66641dcecb3a486c55
Chey <ctenmanikdavid@gmail.com>
2024-02-17 22:47:43 +0000

    removed sales related fields from model

--- customer_accounts/models.py
+++ customer_accounts/models.py
@@ -4,6 +4,7 @@ Importing models in order to define the CustomerAccount model.
 Importing get_user_model to interact with the default User model in Django.
 
 """
+
 from django.core.validators import RegexValidator
 from django.db import models
 from django.contrib.auth import get_user_model
@@ -29,21 +30,46 @@ class CustomerAccount(models.Model):
     the User model directly.
     """
 
-    first_name = models.CharField(max_length=30, blank=True)
-    last_name = models.CharField(max_length=30, blank=True)
+    first_name = models.CharField(
+        max_length=30,
+        blank=True,
+        default="Information not provided",
+    )
+    last_name = models.CharField(
+        max_length=30,
+        blank=True,
+        default="Information not provided",
+    )
 
     phone = models.CharField(
-        max_length=15, validators=[phone_regex], blank=True
+        max_length=15,
+        validators=[phone_regex],
+        blank=True,
     )
 
     # I've kept the character count pretty low because this only dealing
     # with UK addresses, so 60 is more than long enough.
     # The longest town in the uk is in wales and only has 58 letters.
-    address_line_1 = models.CharField(max_length=60, default="Not provided")
-    address_line_2 = models.CharField(max_length=60, default="Not provided")
-    city = models.CharField(max_length=60, default="Not provided")
-    county = models.CharField(max_length=20, default="Not provided")
-    post_code = models.CharField(max_length=15, default="Not provided")
+    address_line_1 = models.CharField(
+        max_length=60,
+        default="Information not provided",
+    )
+    address_line_2 = models.CharField(
+        max_length=60,
+        default="Information not provided",
+    )
+    city = models.CharField(
+        max_length=60,
+        default="Information not provided",
+    )
+    county = models.CharField(
+        max_length=20,
+        default="Information not provided",
+    )
+    post_code = models.CharField(
+        max_length=15,
+        default="Information not provided",
+    )
 
     # Defines the accepted format of dates, when they are put in by the user.
     birth_date = models.DateField(null=True, blank=True)
@@ -55,28 +81,20 @@ class CustomerAccount(models.Model):
         ("O", "Other"),
         ("N", "Prefer not to say"),
     ]
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default="N")
-
-    # non-active until app is extended in functionality.
-    account_number = models.CharField(
-        max_length=20, unique=True, blank=True, null=True
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER_CHOICES,
+        default="N",
     )
+
     registration_date = models.DateTimeField(default=timezone.now)
-
-    # non-active until app is extended in functionality.
-    payment_method = models.CharField(max_length=50, blank=True, null=True)
-
-    # Keeping these set to true so that they allow them to be essentially empty
-    # until the application has extended functionality of ordering in place.
-    purchase_order_date = models.DateTimeField(null=True, blank=True)
-    delivery_order_date = models.DateTimeField(null=True, blank=True)
-
-    # non-active until app is extended in functionality.
-    basket_item_count = models.IntegerField(default=0)
 
     # Both memorable name and memorable date can be used as security questions
     # if talking over the telephone.
-    memorable_name = models.CharField(max_length=60, default="Not provided")
+    memorable_name = models.CharField(
+        max_length=60,
+        default="Information not provided",
+    )
 
     # Defines the accepted format of dates, when they are put in by the user.
     memorable_date = models.DateField(blank=True, null=True)
@@ -85,6 +103,5 @@ class CustomerAccount(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
 
     # Returns the username.
-    # Disabling pylint because the warning is a linter issue and distracting.
     def __str__(self):
-        return self.user.username  # pylint: disable=no-member
+        return self.user.username
