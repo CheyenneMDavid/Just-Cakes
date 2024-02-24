2c26c87c662208ea764b64f60e9abf67d02edd35
Chey <ctenmanikdavid@gmail.com>
2024-02-24 00:28:24 +0000

    Add conditional logic to ensure default cake image in gallery

--- cakes/models.py
+++ cakes/models.py
@@ -1,8 +1,17 @@
+"""
+This models.py is for managing all the data for the cakes app.
+Category, flavours, names and the individual descriptions, none of which are 
+hardcoded, leaving a site admin free to create and add new ones.
+"""
+
 from django.db import models
 from cloudinary.models import CloudinaryField
 
 
 class CakeCategory(models.Model):
+    """
+    Representing the category for cakes
+    """
 
     category = models.CharField(max_length=100, default="to be set")
 
@@ -50,20 +59,24 @@ class IndividualCake(models.Model):
     flavours = models.ManyToManyField(CakeFlavour)
 
 
+# Used for displaying specific cake images when they're setup via the admin
+# screen.
 class CakeImage(models.Model):
     cake = models.ForeignKey(
         IndividualCake, related_name="images", on_delete=models.CASCADE
     )
-
     image = CloudinaryField("image", folder="just_cakes_images")
 
     def __str__(self):
         return f"{self.cake.name} Image"
 
 
+# Used to ensure an image is displayed in case the admin doesn't supply an
+# image for an addition to the gallery.
 class Cake(models.Model):
-    # Define your fields here
-    # For example:
     name = models.CharField(max_length=100)
     description = models.TextField()
-    # Add more fields as needed
+
+    # Used in the conditional logic in the index,html which is the gallery of
+    # cake images.
+    default_image = models.URLField(default="placeholder")
