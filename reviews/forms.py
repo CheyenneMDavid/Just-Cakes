2a00609b5711a93d78cdb48ae165438da81ec816
Chey <ctenmanikdavid@gmail.com>
2024-02-28 04:31:44 +0000

    Added UserCommentForm class

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,17 +1,27 @@
 # Import required modules
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 
 class UserPostForm(forms.ModelForm):
-    """Form for creating and editing user posts, by the user."""
+    """Form for creating and editing user posts"""
 
     class Meta:
         model = Post
-        # The fields that are used by the forms a user fils in when
+        # The fields that are used by the forms a user fills in when
         # submitting a post.
         fields = (
             "title",
             "content",
             "featured_image",
         )
+
+
+class CommentForm(forms.ModelForm):
+    """
+    Form for creating comments on posts.
+    """
+
+    class Meta:
+        model = Comment
+        fields = ("body",)
