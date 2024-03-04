2d4239493a2f0e4139c8659670137a7f1c87a2e0
Chey <ctenmanikdavid@gmail.com>
2024-03-04 15:56:41 +0000

    Add views to handle form submission

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+"""
+Purpose of this view is to handle the rendering of the contact the form page
+and processing of form submissions.
+It renders the contact form page and handles the submissions.
+"""
 
-# Create your views here.
+from django.shortcuts import render
+from .forms import ContactForm
+
+
+def contact_view(request):
+    submitted = False
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            submitted = True
+    else:
+        form = ContactForm()
+    return render(
+        request,
+        "contact.html",
+        {"form": form, "submitted": submitted},
+    )
