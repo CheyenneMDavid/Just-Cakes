f060d4f40735bad088d8bb33bb98c2890ab3313a
Cheyenne David <ctenmanikdavid@gmail.com>
2024-08-20 06:52:53 +0000

    modified settings.json

--- reviews/views.py
+++ reviews/views.py
@@ -30,7 +30,7 @@ class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "reviews/post_list.html"
-    paginate_by = 6
+    paginate_by = 8
 
 
 class PostDetail(View):
